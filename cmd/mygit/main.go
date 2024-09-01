5a1e0dda77323e3d553cab47b18ea3dd65f6f0c9
Edgar Modesto <edgarmodesto23@gmail.com>
2024-09-01 16:19:59 -0700

    codecrafters submit [skip ci]

--- cmd/mygit/main.go
+++ cmd/mygit/main.go
@@ -1,15 +1,34 @@
 package main
 
 import (
+	"bytes"
+	"compress/zlib"
 	"fmt"
+	"io"
 	"os"
 )
 
+func ReadBlob(f *os.File) (string, error) {
+	r, err := zlib.NewReader(f)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+  var encoded []byte
+	defer r.Close()
+	data := new(bytes.Buffer)
+	io.Copy(data, r)
+  for idx, val := range data.Bytes() {
+    if val == 0 {
+      encoded = data.Bytes()[idx + 1:]
+    }
+  }
+  res := fmt.Sprintf("%s", encoded)
+	return res, nil
+}
+
 // Usage: your_program.sh <command> <arg1> <arg2> ...
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
-
 	if len(os.Args) < 2 {
 		fmt.Fprintf(os.Stderr, "usage: mygit <command> [<args>...]\n")
 		os.Exit(1)
@@ -29,6 +48,23 @@ func main() {
 		}
 
 		fmt.Println("Initialized git directory")
+	case "cat-file":
+		if os.Args[2] == "-p" {
+			dir := os.Args[3][:2]
+			file := os.Args[3][2:]
+			path := fmt.Sprintf(".git/objects/%v/%v", dir, file)
+			f, err := os.Open(path)
+			defer f.Close()
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
+      data, err := ReadBlob(f)
+      if err != nil {
+        panic(err)
+      }
+      fmt.Print(data)
+		}
 
 	default:
 		fmt.Fprintf(os.Stderr, "Unknown command %s\n", command)
